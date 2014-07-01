32b1b0b0b28b4f3519a16bd541dd5aaa46cf9db6
Guadalupe <g_lupita_1992@hotmail.com>
2014-06-30 23:07:52 -0500

    'sesioness'

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,38 @@
 class UserController < ApplicationController
-  def index
+def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
-  end
-
+    @user = User.new
+  end 
+  
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url#, :notice => "Se ha creado el usuario con exito"
+    else
+      render :new
+    end
+  end
+   
+  def destroy
+    logout
+    redirect_to root_url
+    #, :notice => "Logged out!"
   end
 
   def edit
   end
 
   def update
-  end
+  end  
 
-  def destroy
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :nombre, :apellidos, :direccion, :telefono, :tipoUsuario)
   end
 end
